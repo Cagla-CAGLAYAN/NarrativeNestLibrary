0980fa7abd9dd80e429392d61263d6ab4d341018
Çağla ÇAĞLAYAN <106310863+Cagla-CAGLAYAN@users.noreply.github.com>
2024-12-18 17:59:46 +0300

    ApplicationDbContext.cs içinde Books tablosu için bookIdyi primarykey olarak atadım. HomeController içinde AddBook fonksiyonu ekledim. Book.cs içinde sql tablosunda uyumsuzluk yaşandığı için Book 'u Books olarak değiştirdim

--- NarrativeNest/Controllers/HomeController.cs
+++ NarrativeNest/Controllers/HomeController.cs
@@ -1,18 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NarrativeNest.Data;
 using NarrativeNest.Models;
+using NarrativeNest.Models.Entities;
 using System.Diagnostics;
 
 namespace NarrativeNest.Controllers
 {
     public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+    { private readonly ApplicationDbContext _context;
+    
+       /* private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
+        }*/
+        public HomeController(ApplicationDbContext context)
+        {
+            _context = context;
         }
-
         public IActionResult Index()
         {
             return View();
@@ -21,6 +28,35 @@ namespace NarrativeNest.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        public IActionResult GetBook(int bookId)
+        {
+            var bookIdGuid = new Guid(bookId.ToString());
+            var bookById = _context.Books.FirstOrDefault(b => b.BookId == bookIdGuid);
+
+            if (bookById == null)
+            {
+                return NotFound("Kitap ID'sine sahip bir kitap bulunamadý.");
+            }
+
+            return Ok(bookById);
+        }
+        public IActionResult AddBook()
+        {
+            var newBook = new Books
+            {
+                Title = "Kitap Adý Örneði",
+                Author = "Yazar Adý Örneði",
+                Genre = "History",
+                BookStatus = "Free",
+                PageNumber = 250
+
+            };
+
+            _context.Books.Add(newBook);
+            _context.SaveChanges();
+            return Ok("Kitap Eklendi");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
